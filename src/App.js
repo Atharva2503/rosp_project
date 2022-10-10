9f39219c7378434864bf10a6bfb7e82c4479f44e
atharva <atharvasbhagwat@gmail.com>
2022-10-10 15:36:33 +0530

    Fina commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+
 
 function App() {
+  const[name,setName]= useState(" ");
+  const [dim_min,setDimn]= useState(0);
+  const [dim_max,setDimx]= useState(0);
+  const [rel_vel,setRelvel]= useState(0);
+  const [miss_dist,setMissdist]= useState(0);
+  const [abs_magnitude,setAbsmagnitude]= useState(0);
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form name='A' className='Form'>
+    <table>
+
+      <tr>
+        <td><strong>ID:</strong></td>
+        <td><input class="IN" type="text"  /></td>
+      </tr>
+      <tr>
+        <td><strong>Name:</strong></td>
+        <td><input class="IN" type="text" id='name' onChange={(name)=>setName(name.target.value)} required/></td>
+      </tr>
+      <tr>
+        <td><strong>Estimated Diameter Min:</strong></td>
+        <td><input class="IN" type="text" id='dim_min' onChange={(name)=>setDimn(name.target.value)} required/></td>
+      </tr>
+      <tr>
+        <td><strong>Estimated Diameter Max:</strong></td>
+        <td><input class="IN" type="text" id='dim_max' onChange={(name)=>setDimx(name.target.value)} required/></td>
+      </tr>
+      <tr>
+        <td><strong>Relative Velocity:</strong></td>
+        <td><input class="IN" type="text" id='rel_vel' onChange={(name)=>setRelvel(name.target.value)} required/></td>
+      </tr>
+      <tr>
+        <td><strong>Miss Distance:</strong></td>
+        <td><input class="IN" type="text" id='miss_dist' onChange={(name)=>setMissdist(name.target.value)} required/></td>
+      </tr>
+      <tr>
+        <td><strong>Orbiting Body:</strong></td>
+        <td><input class="IN" type="text"/></td>
+      </tr>
+      <tr>
+        <td><strong>Sentry Object:</strong></td>
+        <td><input class="IN" type="text"/></td>
+      </tr>
+      <tr>
+        <td><strong>Absolute Magnitude:</strong></td>
+        <td><input class="IN" type="text" id='abs_magnitude' onChange={(name)=>setAbsmagnitude(name.target.value)} required/></td>
+      </tr>
+      <tr>
+        <td><button id='b1' type='Reset'>Clear</button></td>
+        <td><button id='b2' type='button' 
+    onClick={()=>{axios.post("http://127.0.0.1:8000/test/",{data:0}).then(data=>console.log(data.data))}}>Submit</button></td>
+      </tr>
+    </table>
+      </form>
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import './Nav.css'
+function Nav(){
+    return(
+        
+           <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <h1 class="display-4">Nasa Nearest Earth Objects Classifier</h1>
+                    <p class="lead">On basis of attributes of object classifier will predict if object is Hazardeous or Not.</p>
+                </div>
+            </div>
+       
+    );
+}
+export default Nav;
